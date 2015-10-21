407a5112ab430db6f23fc4317a7400ada797d274
Fabian Pedregosa <f@bianp.net>
2015-10-21 09:45:35 +0200

    make example simpler

--- examples/bench.py
+++ examples/bench.py
@@ -1,16 +1,20 @@
-from sklearn import linear_model, cross_validation, datasets, grid_search
+from sklearn import linear_model, cross_validation, datasets,\
+    metrics
 import mord
 import numpy as np
 
-for n_samples in range(40, 200, 20):
-    X, y = datasets.make_classification(n_samples=n_samples, n_features=10000,
-        n_classes=5, n_informative=5)
-    clf1 = linear_model.LogisticRegressionCV(solver='lbfgs',
-                                            multi_class='multinomial')
+boston = datasets.load_boston()
+X, y = boston.data, np.round(boston.target).astype(np.int)
+y -= y.min()
 
-    clf2 = grid_search.GridSearchCV(mord.LogisticIT(), {'alpha': np.logspace(
-        -3, 3, 10)})
-    cv = cross_validation.StratifiedShuffleSplit(y, test_size=0.3)
-    print(cross_validation.cross_val_score(clf1, X, y, cv=cv).mean())
-    print(cross_validation.cross_val_score(clf2, X, y, cv=cv).mean())
-    print()
+clf1 = linear_model.LogisticRegression(
+    solver='lbfgs', multi_class='multinomial')
+clf1.fit(X, y)
+print('Mean Absolute Error of LogisticRegression: %s' %
+      metrics.mean_absolute_error(clf1.predict(X), y))
+
+
+clf2 = mord.LogisticAT(alpha=1.)
+clf2.fit(X, y)
+print('Mean Absolute Error of LogisticAT %s' %
+      metrics.mean_absolute_error(clf2.predict(X), y))
