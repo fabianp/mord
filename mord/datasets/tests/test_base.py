a429a5385444e40e351a2ba2d74149bb778fb959
Peter Gedeck <peter.gedeck@gmail.com>
2023-04-26 10:19:57 -0400

    Add github workflow

--- mord/datasets/tests/test_base.py
+++ mord/datasets/tests/test_base.py
@@ -1,6 +1,3 @@
-# from sklearn.utils.testing import assert_true
-# from sklearn.utils.testing import assert_equal
-
 from mord.datasets.base import load_housing
 
 
@@ -10,7 +7,3 @@ def test_load_housing():
     assert res.target.size == 1681
     assert len(res.feature_names) == 3
     assert res.DESCR
-    # assert_equal(res.data.shape, (1681, 3))
-    # assert_equal(res.target.size, 1681)
-    # assert_equal(len(res.feature_names), 3)
-    # assert_true(res.DESCR)
